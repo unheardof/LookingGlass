9aa72795f924dc2b2745984682a014c58da17218
Timothy Heard <timheard@DESKTOP-ID414B5.localdomain>
2023-06-05 00:34:31 -0500

    Created script to run the HTTPS container

---
+++ bin/run_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the root project directory
+cd $(dirname "${BASH_SOURCE[0]}")
+cd ../..
+
+APP="docker.looking_glass"
+
+#
+# Bind port 443 on the host machine to port 443 on the container
+#
+# If container already exists, use the following command instead:
+#
+#     docker start docker.looking_glass
+#
+docker run -d -p 443:443 --name=${APP} -v $PWD:/app ${APP}
