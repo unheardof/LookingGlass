768e82b8a7d6d65f986f694cc3227d9cda66627e
EC2 Default User <ec2-user@ip-172-31-28-144.us-east-2.compute.internal>
2023-06-06 22:09:50 +0000

    Ensure the db/ directory exists

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ ! -f "$SCRIPT_DIR/../db/password.txt" ]; then
     read -p "Enter desired DB password: " db_password
+    mkdir "$SCRIPT_DIR/../db"
     echo "$db_password" > "$SCRIPT_DIR/../db/password.txt"
 fi
 
